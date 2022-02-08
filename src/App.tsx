be41f292fe88729ce946418c9dac41d98a8d4cac
Heithwald <oleg.barbasov@gmail.com>
2022-02-08 03:18:18 +0300

    added proxyURL

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,9 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 const App = ({ children }: any) => {
   const dispatch = useDispatch();
 
-  const URL =
-    "https://newsapi.org/v2/everything?q=education&apiKey=d8164f53df774c6eaf90a07f705ecc01";
+  const proxyURL = "https://cors-anywhere.herokuapp.com/";
+
+  const URL = `${proxyURL}https://newsapi.org/v2/everything?q=education&apiKey=d8164f53df774c6eaf90a07f705ecc01`;
 
   useEffect(() => {
     axios.get(URL).then((response) => {
